62b361ff9344d327b8a15bc5be09bfd00b612ed5
Iago Leão <iago.leao@remessaonline.com.br>
2021-07-08 17:33:51 -0300

    finish docker run local

--- run_local.sh
+++ run_local.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 SERVICE_PATH=$1;
+SERVICE_PORT=$2;
 SERVICE_PATH_ARR=(${SERVICE_PATH//\// })
 
 SERVICE_NAME=${SERVICE_PATH_ARR[1]}
@@ -10,5 +11,5 @@ docker build -f Dockerfile -t baseimage .
 
 cd $SERVICE_PATH;
 
-docker build -f Dockerfile -t $SERVICE_NAME | tr '[:upper:]' '[:lower:]' .
-docker run --rm -e PORT=3002 -p 3002:3002 ${SERVICE_NAME}
+docker build -f Dockerfile -t $SERVICE_NAME .
+docker run --rm -e PORT=${SERVICE_PORT} -p ${SERVICE_PORT}:${SERVICE_PORT} ${SERVICE_NAME}
